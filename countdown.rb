858ac5cc738a875fecea305c5f6c10e9ecba63c9
Alicia Santiago <albrown07@mac.com>
2019-02-08 15:25:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  num = 10
+   while num > 0
+    puts "#{num} SECOND(S)!"
+   num -= 1
+ end
+ puts "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num)
+   num = 5
+   while num > 0
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+   num -= 1
+ end
 end
